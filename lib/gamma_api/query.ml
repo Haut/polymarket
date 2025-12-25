158bc9fd028922461f4884037753f118627c9937
Richard <rkhaut1@gmail.com>
2025-12-25 10:50:56 -0500

    refactor: remove unused PPX derivers from query types and enable warning
    - Remove [@@deriving yojson, show, eq] from query enums since only
      string_of_* functions are actually used
    - Enable warning +32 (unused value declarations) in lib/dune

--- lib/gamma_api/query.ml
+++ lib/gamma_api/query.ml
@@ -8,7 +8,6 @@ type status =
   | Active  (** Only active/open items *)
   | Closed  (** Only closed/resolved items *)
   | All  (** All items regardless of status *)
-[@@deriving yojson, show, eq]
 
 let string_of_status = function
   | Active -> "active"
@@ -19,7 +18,6 @@ type parent_entity_type =
   | Event  (** Event entity *)
   | Series  (** Series entity *)
   | Market  (** Market entity *)
-[@@deriving yojson, show, eq]
 
 let string_of_parent_entity_type = function
   | Event -> "Event"
@@ -30,6 +28,5 @@ type slug_size =
   | Sm  (** Small slug *)
   | Md  (** Medium slug *)
   | Lg  (** Large slug *)
-[@@deriving yojson, show, eq]
 
 let string_of_slug_size = function Sm -> "sm" | Md -> "md" | Lg -> "lg"
--- lib/gamma_api/query.mli
+++ lib/gamma_api/query.mli
@@ -11,11 +11,6 @@ type status =
   | All  (** All items regardless of status *)
 
 val string_of_status : status -> string
-val status_of_yojson : Yojson.Safe.t -> status
-val yojson_of_status : status -> Yojson.Safe.t
-val pp_status : Format.formatter -> status -> unit
-val show_status : status -> string
-val equal_status : status -> status -> bool
 
 (** Parent entity type for comments *)
 type parent_entity_type =
@@ -24,11 +19,6 @@ type parent_entity_type =
   | Market  (** Market entity *)
 
 val string_of_parent_entity_type : parent_entity_type -> string
-val parent_entity_type_of_yojson : Yojson.Safe.t -> parent_entity_type
-val yojson_of_parent_entity_type : parent_entity_type -> Yojson.Safe.t
-val pp_parent_entity_type : Format.formatter -> parent_entity_type -> unit
-val show_parent_entity_type : parent_entity_type -> string
-val equal_parent_entity_type : parent_entity_type -> parent_entity_type -> bool
 
 (** Slug size for URL slugs *)
 type slug_size =
@@ -37,8 +27,3 @@ type slug_size =
   | Lg  (** Large slug *)
 
 val string_of_slug_size : slug_size -> string
-val slug_size_of_yojson : Yojson.Safe.t -> slug_size
-val yojson_of_slug_size : slug_size -> Yojson.Safe.t
-val pp_slug_size : Format.formatter -> slug_size -> unit
-val show_slug_size : slug_size -> string
-val equal_slug_size : slug_size -> slug_size -> bool
