c381830954ce7051063de577a25c980b4c286fc5
Richard <rkhaut1@gmail.com>
2025-12-28 08:20:09 -0500

    refactor: hierarchical functors for CLOB client operations
    - Make_public: 7 public endpoints (Unauthed)
    - Make_l1_ops: includes Make_public + L1 ops (L1)
    - Make_l2_ops: includes Make_l1_ops + L2 ops (L2)
    - Each client module now has just create + one include

--- lib/clob_api/client.ml
+++ lib/clob_api/client.ml
@@ -26,6 +26,43 @@ module Make_public (M : HAS_HTTP) = struct
   let get_price_history (t : M.t) = Endpoints.get_price_history (M.http t)
 end
 
+(** {1 L1 Operations Functor} *)
+
+module type HAS_L1_AUTH = sig
+  type t
+  type l2_client
+
+  val http : t -> H.t
+  val private_key : t -> Crypto.private_key
+  val address : t -> string
+
+  val make_l2 :
+    H.t -> Crypto.private_key -> string -> Auth.credentials -> l2_client
+end
+
+module Make_l1_ops (M : HAS_L1_AUTH) = struct
+  include Make_public (M)
+
+  let address (t : M.t) = M.address t
+
+  let create_api_key (t : M.t) ~nonce =
+    Endpoints.create_api_key (M.http t) ~private_key:(M.private_key t)
+      ~address:(M.address t) ~nonce
+
+  let derive_api_key (t : M.t) ~nonce =
+    match
+      Endpoints.derive_api_key (M.http t) ~private_key:(M.private_key t)
+        ~address:(M.address t) ~nonce
+    with
+    | Ok resp ->
+        let credentials = Auth.credentials_of_derive_response resp in
+        let l2_client =
+          M.make_l2 (M.http t) (M.private_key t) (M.address t) credentials
+        in
+        Ok (l2_client, resp)
+    | Error e -> Error e
+end
+
 (** {1 Client Types} *)
 
 type unauthed = { http : H.t }
@@ -37,6 +74,67 @@ type l2 = {
   address : string;
   credentials : Auth.credentials;
 }
+
+let make_l2 http private_key address credentials =
+  { http; private_key; address; credentials }
+
+(** {1 L2 Operations Functor} *)
+
+module type HAS_L2_AUTH = sig
+  include HAS_L1_AUTH
+
+  val credentials : t -> Auth.credentials
+end
+
+module Make_l2_ops (M : HAS_L2_AUTH) = struct
+  include Make_l1_ops (M)
+
+  let credentials (t : M.t) = M.credentials t
+
+  let delete_api_key (t : M.t) =
+    Endpoints.delete_api_key (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let get_api_keys (t : M.t) =
+    Endpoints.get_api_keys (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let create_order (t : M.t) =
+    Endpoints.create_order (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let create_orders (t : M.t) =
+    Endpoints.create_orders (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let get_order (t : M.t) =
+    Endpoints.get_order (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let get_orders (t : M.t) =
+    Endpoints.get_orders (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let cancel_order (t : M.t) =
+    Endpoints.cancel_order (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let cancel_orders (t : M.t) =
+    Endpoints.cancel_orders (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let cancel_all (t : M.t) =
+    Endpoints.cancel_all (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let cancel_market_orders (t : M.t) =
+    Endpoints.cancel_market_orders (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+
+  let get_trades (t : M.t) =
+    Endpoints.get_trades (M.http t) ~credentials:(M.credentials t)
+      ~address:(M.address t)
+end
 
 (** {1 Unauthenticated Client} *)
 
@@ -65,34 +163,14 @@ module L1 = struct
     let address = Crypto.private_key_to_address private_key in
     { http; private_key; address }
 
-  let address (t : t) = t.address
-
-  let create_api_key (t : t) ~nonce =
-    Endpoints.create_api_key t.http ~private_key:t.private_key
-      ~address:t.address ~nonce
-
-  let derive_api_key (t : t) ~nonce =
-    match
-      Endpoints.derive_api_key t.http ~private_key:t.private_key
-        ~address:t.address ~nonce
-    with
-    | Ok resp ->
-        let credentials = Auth.credentials_of_derive_response resp in
-        let l2_client : l2 =
-          {
-            http = t.http;
-            private_key = t.private_key;
-            address = t.address;
-            credentials;
-          }
-        in
-        Ok (l2_client, resp)
-    | Error e -> Error e
-
-  include Make_public (struct
+  include Make_l1_ops (struct
     type t = l1
+    type l2_client = l2
 
     let http (t : t) = t.http
+    let private_key (t : t) = t.private_key
+    let address (t : t) = t.address
+    let make_l2 = make_l2
   end)
 end
 
@@ -107,55 +185,15 @@ module L2 = struct
     let address = Crypto.private_key_to_address private_key in
     { http; private_key; address; credentials }
 
-  let address (t : t) = t.address
-  let credentials (t : t) = t.credentials
-
-  (* L1 operations *)
-  let create_api_key (t : t) ~nonce =
-    Endpoints.create_api_key t.http ~private_key:t.private_key
-      ~address:t.address ~nonce
-
-  let delete_api_key (t : t) =
-    Endpoints.delete_api_key t.http ~credentials:t.credentials
-      ~address:t.address
-
-  let get_api_keys (t : t) =
-    Endpoints.get_api_keys t.http ~credentials:t.credentials ~address:t.address
-
-  (* L2 order operations *)
-  let create_order (t : t) =
-    Endpoints.create_order t.http ~credentials:t.credentials ~address:t.address
-
-  let create_orders (t : t) =
-    Endpoints.create_orders t.http ~credentials:t.credentials ~address:t.address
-
-  let get_order (t : t) =
-    Endpoints.get_order t.http ~credentials:t.credentials ~address:t.address
-
-  let get_orders (t : t) =
-    Endpoints.get_orders t.http ~credentials:t.credentials ~address:t.address
-
-  let cancel_order (t : t) =
-    Endpoints.cancel_order t.http ~credentials:t.credentials ~address:t.address
-
-  let cancel_orders (t : t) =
-    Endpoints.cancel_orders t.http ~credentials:t.credentials ~address:t.address
-
-  let cancel_all (t : t) =
-    Endpoints.cancel_all t.http ~credentials:t.credentials ~address:t.address
-
-  let cancel_market_orders (t : t) =
-    Endpoints.cancel_market_orders t.http ~credentials:t.credentials
-      ~address:t.address
-
-  let get_trades (t : t) =
-    Endpoints.get_trades t.http ~credentials:t.credentials ~address:t.address
-
-  (* Public operations *)
-  include Make_public (struct
+  include Make_l2_ops (struct
     type t = l2
+    type l2_client = l2
 
     let http (t : t) = t.http
+    let private_key (t : t) = t.private_key
+    let address (t : t) = t.address
+    let make_l2 = make_l2
+    let credentials (t : t) = t.credentials
   end)
 end
 
